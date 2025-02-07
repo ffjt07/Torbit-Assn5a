34cab56e4624f6d7f9662f64ef5642a23af64e02
JT <jstorbit@gmail.com>
2025-02-06 21:11:22 -0700

    updated logic for submit generated error window, still debugging

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,60 +1,82 @@
-var openB = true;
-var openH = true;
+var type2 = document.getElementById("type2");
+var session2 = document.getElementsByName("session-2");
+var evol3 = document.getElementById("evol3");
+var legend2 = document.getElementById("legend2");
+var form = document.getElementById("registration-form");
+var errorMsgB = "Cannot select any Pokémon Evolution sessions if Electric, Ground, Rock, Steel, Ice, Fighting Pokémon session is selected.";
+var errorMsgH = "Cannot select Two Series Legendary Pokémon session unless Double Evolution session is also selected.";
+var errMessages = 0;
+var errWinOpen = false;
 
-function workshopB() {
-    const type2 = document.getElementById("type2");
-    const session2 = document.getElementsByName("session-2");
-
-    for (let i = 0; i < session2.length; ++i) {
-        if (session2[i].checked && type2.checked) {
-            clearRadio(session2);
-            if (openB) {
-                errWindow("B");
+if (document.title === "Registration Form") {
+    console.log(document.title);
+    form.addEventListener("submit", function (event) {
+        if (type2.checked) {
+            if (!errWinOpen) {
+                if (sessionTwoCheck() == true) {
+                    event.preventDefault();
+                    if (legend2.checked) {
+                        errMessages = 2;
+                        legend2.checked = false;
+                    }
+                    else {
+                        errMessages = 1;
+                    }
+                    errWindow();
+                }
             }
         }
+        else if (legend2.checked && !evol3.checked) {
+            if (!errWinOpen) {
+                event.preventDefault();
+                errWindow();
+                errMessages = 3;
+                legend2.checked = false;
+            }
+        }
+    });
+}
+
+function errWindow() {
+    const winHeight = 400;
+    const winWidth = 500;
+    let topOffset = screenY;
+    let leftOffset = screenX;
+    let top = topOffset + (outerHeight - winHeight) / 2;
+    let left = leftOffset + (outerWidth - winWidth) / 2;
+    let regerrorUrl = "./regerror.html";
+    let windowParams = `resizable=no,loacation=yes,width=${winWidth}, height=${winHeight}, left=${left}, top=${top}`;
+    var errorWindow = window.open(regerrorUrl, "Session Error", windowParams);
+    let errorHeaderOne = errorWindow.document.getElementById("error1");
+    let errorHeaderTwo = errorWindow.document.getElementById("error2");
+    console.log(errorWindow.location);
+
+    if (errMessages == 1) {
+        errorHeaderOne.textContent = errorMsgB;
+    }
+    else if (errMessages == 2) {
+        errorHeaderOne.textContent = errorMsgB;
+        errorHeaderTwo.textContent = errorMsgH;
+    }
+    else if (errMessages == 3) {
+        errorHeaderOne.textContent = errorMsgH;
     }
 }
 
-function workshopH() {
-    const evol3 = document.getElementById("evol3");
-    const legend2 = document.getElementById("legend2");
-
-    if (!evol3.checked && legend2.checked) {
-        legend2.checked = false;
-        if (openH) {
-            errWindow("H");
+function sessionTwoCheck() {
+    for (let i = 0; i < session2.length; ++i) {
+        if (session2[i].checked) {
+            clearRadio(session2);
+            return true;
         }
     }
+    return false;
 }
 
 function clearRadio(radioObject) {
     for (let i = 0; i < radioObject.length; ++i) {
         radioObject[i].checked = false;
     }
-}
-
-function errWindow(workshop) {
-    const winHeight = 400;
-    const winWidth = 500;
-    var top = (window.innerHeight - winHeight) / 2;
-    var left = (window.innerWidth -winWidth) / 2;
-    var berrorUrl = "berror.html";
-    var herrorUrl = "herror.html";
-    var errorWindow = window;
-    var windowParams = `width=${winWidth}, height=${winHeight}, left=${left}, top=${top}`;
-    if (workshop == "B") {
-        errorWindow.open(berrorUrl, "Error Window", windowParams);
-        openB = false;
-    }
-    else {
-        errorWindow.open(herrorUrl, "Error Window", windowParams);
-        openH = false;
-    }
-}
-
-function closeWin() {
-    let currWin = open(location, '_self');
-    currWin.close();
 }
 
 function evolCheck() {
@@ -74,3 +96,10 @@ function thankYouAlert() {
         }
     }
 }
+
+function closeWin() {
+        let currWin = open(location, '_self');
+        errWinOpen = false;
+        currWin.close();
+}
+
