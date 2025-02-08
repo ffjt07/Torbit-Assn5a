dd38594d3200c8d17c1114e5f09f2cc839d747d0
JT <jstorbit@gmail.com>
2025-02-07 21:27:53 -0700

    updated to jquery and local storage for error window handling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,105 +1,135 @@
-var type2 = document.getElementById("type2");
-var session2 = document.getElementsByName("session-2");
-var evol3 = document.getElementById("evol3");
-var legend2 = document.getElementById("legend2");
-var form = document.getElementById("registration-form");
-var errorMsgB = "Cannot select any Pokémon Evolution sessions if Electric, Ground, Rock, Steel, Ice, Fighting Pokémon session is selected.";
-var errorMsgH = "Cannot select Two Series Legendary Pokémon session unless Double Evolution session is also selected.";
-var errMessages = 0;
-var errWinOpen = false;
+var type2 = $('#type2');
+var evol1 = $('#evol1');
+var evol2 = $('#evol2');
+var evol3 = $("#evol3");
+var legend2 = $("#legend2");
+var errorNode = $(".error-win")
+var regSubmit = $("#reg-button");
+var regForm = $("#registration-form");
+var poll = $('input[name="poll"]');
+var session2 = $('input[name="session-2"]');
+var evolErrorMsg = "Cannot select any Pokémon Evolution sessions if Electric, Ground, Rock, Steel, Ice, Fighting Pokémon session is selected.";
+var legErrorMsg = "Cannot select Two Series Legendary Pokémon session unless Double Evolution session is also selected.";
+var errWinOpen = 'open';
+var errMsg = 'err';
 
 if (document.title === "Registration Form") {
-    console.log(document.title);
-    form.addEventListener("submit", function (event) {
-        if (type2.checked) {
-            if (!errWinOpen) {
-                if (sessionTwoCheck() == true) {
-                    event.preventDefault();
-                    if (legend2.checked) {
-                        errMessages = 2;
-                        legend2.checked = false;
-                    }
-                    else {
-                        errMessages = 1;
-                    }
-                    errWindow();
-                }
-            }
-        }
-        else if (legend2.checked && !evol3.checked) {
-            if (!errWinOpen) {
-                event.preventDefault();
-                errWindow();
-                errMessages = 3;
-                legend2.checked = false;
-            }
-        }
-    });
+    localStorage.setItem(errWinOpen, 'false');
 }
 
-function errWindow() {
+if (document.title === "Session Error") {
+    var h3el = $('<h3>');
+    if (localStorage.getItem(errMsg) === 'B') {
+        h3el.text(evolErrorMsg);
+        errorNode.prepend(h3el);
+    }
+    else {
+        h3el.text(legErrorMsg);
+        errorNode.prepend(h3el);
+    }
+}
+    
+function workshopCheck() {
+    if (type2.is(":checked")) {
+        if (sessionTwoCheck() === true) {
+            clearRadio(session2);
+            if (localStorage.getItem(errWinOpen) === 'false') {
+                errWindow("B");
+            }
+        }
+        else if (legend2.is(":checked")) {
+            if (localStorage.getItem(errWinOpen) === 'false') {
+                legend2.prop("checked", false);
+                errWindow("H");
+            }
+        }
+    }
+    if (legend2.is(":checked") && !evol3.is(":checked")) {
+        if (localStorage.getItem(errWinOpen) === 'false') {
+            legend2.prop("checked", false);
+            errWindow("H");
+        }
+    }
+}
+
+
+function errWindow(workshop) {
     const winHeight = 400;
     const winWidth = 500;
     let topOffset = screenY;
     let leftOffset = screenX;
     let top = topOffset + (outerHeight - winHeight) / 2;
     let left = leftOffset + (outerWidth - winWidth) / 2;
-    let regerrorUrl = "./regerror.html";
+    let regerrorUrl = "regerror.html";
+    let legenderrorUrl = "legenderror.html";
     let windowParams = `resizable=no,loacation=yes,width=${winWidth}, height=${winHeight}, left=${left}, top=${top}`;
-    var errorWindow = window.open(regerrorUrl, "Session Error", windowParams);
-    let errorHeaderOne = errorWindow.document.getElementById("error1");
-    let errorHeaderTwo = errorWindow.document.getElementById("error2");
-    console.log(errorWindow.location);
-
-    if (errMessages == 1) {
-        errorHeaderOne.textContent = errorMsgB;
+    var errorWindow = window;
+    
+    localStorage.setItem(errWinOpen, 'true');
+    if (workshop === "B") {
+        localStorage.setItem(errMsg, 'B');
     }
-    else if (errMessages == 2) {
-        errorHeaderOne.textContent = errorMsgB;
-        errorHeaderTwo.textContent = errorMsgH;
+    else {
+        localStorage.setItem(errMsg, 'H');
     }
-    else if (errMessages == 3) {
-        errorHeaderOne.textContent = errorMsgH;
-    }
+    errorWindow.open(regerrorUrl, "Error Window", windowParams);
 }
 
 function sessionTwoCheck() {
-    for (let i = 0; i < session2.length; ++i) {
-        if (session2[i].checked) {
-            clearRadio(session2);
-            return true;
+    isChecked = false;
+    session2.each(function () {
+        if ($(this).is(":checked")) {
+            isChecked = true;
+            return false;
         }
-    }
-    return false;
+    });
+    return isChecked;
 }
 
 function clearRadio(radioObject) {
-    for (let i = 0; i < radioObject.length; ++i) {
-        radioObject[i].checked = false;
-    }
+    $(radioObject).prop("checked", false);
 }
 
 function evolCheck() {
-    const type2 = document.getElementById("type2");
-    const evol1 = document.getElementById("evol1");
-
-    if (type2.checked) {
-        evol1.removeAttribute("required");
+    if (type2.is(":checked")) {
+        evol1.removeAttr("required");
     }
 }
 
 function thankYouAlert() {
-    const poll = document.getElementsByName("poll");
-    for (let i = 0; i < poll.length; ++i) {
-        if (poll[i].checked) {
-            alert("Thank you for voting for: " + poll[i].value);
+    poll.each(function () {
+        if ($(this).is(":checked")) {
+            alert("Thank you for voting for: " + $(this).val());
         }
-    }
+    });
 }
 
 function closeWin() {
         let currWin = open(location, '_self');
-        errWinOpen = false;
+        localStorage.setItem(errWinOpen, 'false');
         currWin.close();
 }
 
+type2.on('click', function () {
+    workshopCheck();
+});
+legend2.on('click', function () {
+    workshopCheck();
+});
+evol1.on('click', function () {
+    workshopCheck();
+});
+evol2.on('click', function () {
+    workshopCheck();
+});
+evol3.on('click', function () {
+    workshopCheck();
+});
+regSubmit.on('click', function () {
+    evolCheck();
+});
+
+if (document.title === "Thank You") {
+    localStorage.removeItem(errWinOpen);
+    localStorage.removeItem(errMsg);
+}
